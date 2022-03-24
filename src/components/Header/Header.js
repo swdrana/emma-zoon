28b3d615d13f98b6b57b80b799564bb19f49324c
swdrana <tctr2s@gmail.com>
2022-03-24 17:52:25 +0600

    header section and nav item work finish

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
         <div>
-            <h1>This my Header</h1>
+            <header>
+                <a href="/"><img src={logo} alt="logo" /></a>
+                <nav>
+                    <ul>
+                        <li><a href="/Order">Order</a></li>
+                        <li><a href="/Review">Review</a></li>
+                        <li><a href="/Inventory">Inventory</a></li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     );
 };
